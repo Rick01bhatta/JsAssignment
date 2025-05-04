f5e49dd6b25c3f734e59a98f678163fb51ae5244
Rick Bhattacharya <rickbhattacharya8@gmail.com>
2025-05-04 21:57:39 +0530

    Add reverse string logic

--- reverseString.js
+++ reverseString.js
@@ -1,1 +1,18 @@
+const ps=require("prompt-sync")
+const prompt=ps({sigint:true})
 
+const input=prompt("enter a string:")
+
+
+
+function reversedString(input){
+    let reversed=""
+        for (let i= input.length-1; i >=0; i--) {
+            
+            reversed=reversed+input[i]
+            
+        }
+        return reversed
+}
+
+console.log("Reverse String:", reversedString(input))
