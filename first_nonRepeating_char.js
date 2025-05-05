f514a7d240401a00635582eb971ee4692aaaf8b6
Rick01bhatta <163346947+Rick01bhatta@users.noreply.github.com>
2025-05-05 12:51:52 +0530

    Update first_nonRepeating_char.js

--- first_nonRepeating_char.js
+++ first_nonRepeating_char.js
@@ -1,1 +1,11 @@
+const ps=require("prompt-sync")
+const prompt=ps({sigint:true})
 
+let userInput=prompt("Enter a string:")
+
+for(let i=0;i<userInput.length;i++){
+    if(userInput.indexOf(userInput[i])===userInput.lastIndexOf(userInput[i])){
+        console.log(userInput[i])
+        break;
+    }
+}
